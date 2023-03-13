5afd1bac6ce7cb047870846ea1ddc104300fa0ef
Rui Oliveira <ruimigueloliveira92@gmail.com>
2023-03-13 19:21:44 +0000

    asd

--- src/pages/Everything/Everything.jsx
+++ src/pages/Everything/Everything.jsx
@@ -2,18 +2,18 @@ import Home from "../Home/Home";
 import React from "react";
 import Cuisine from "../Cuisine/Cuisine"
 import Searched from "../Searched/Searched"
-import { Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Recipe from "../Recipe/Recipe";
 
 const Pages = () => {
 
     return (
-        <Routes basename="/recipe">
+        <BrowserRouter basename="/recipe">
             <Route path="/" element={<Home />} />
             <Route path="/cuisine/:type" element={<Cuisine />} />
             <Route path="/searched/:search" element={<Searched />} />
             <Route path="/recipe/:name" element={<Recipe />} />
-        </Routes>
+        </BrowserRouter>
     )
 };
 
